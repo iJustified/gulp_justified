dd65730c3d85be7d59e8372f6023ac0045f374fc
Ilya Tsygankov <ijustified@yandex.ru>
2022-01-04 21:21:17 +0300

    cancel update

--- gulpfile.js
+++ gulpfile.js
@@ -1,81 +1,77 @@
-const { src, dest, watch, parallel, series } = require('gulp');
+const gulp = require('gulp');
+const del = require('del');
+// pug потом можно подключить
+const gulpSass = require('gulp-sass')(require('sass'));
+const gulpAutoprefixer = require('gulp-autoprefixer');
+// gulp babel потом можно подключить
+const gulpImageMin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
 
-const scss           = require('gulp-sass')(require('sass'));
-const browserSync    = require('browser-sync').create();
-const autoprefixer   = require('gulp-autoprefixer');
-const imagemin       = require('gulp-imagemin');
-const del            = require('del');
 
+function delBuild() {
+   return del('build');
+}
 
-function browsersync() {
+function fonts(){
+   return gulp.src('#dev/fonts/**/*.*')
+      .pipe(gulp.dest('build/fonts'));
+}
+
+function html() {
+   return gulp.src('#dev/index.html')
+      .pipe(gulp.dest('build'));
+}
+
+function normalize() {
+   return gulp.src('#dev/scss/normalize.css')
+      .pipe(gulp.dest('build/css'));
+}
+
+function scss2css() {
+   return gulp.src('#dev/scss/style.scss')
+      .pipe(gulpSass())
+      .pipe(gulpAutoprefixer())
+      .pipe(browserSync.stream())
+      .pipe(gulp.dest('build/css'));
+}
+
+function script() {
+   return gulp.src('#dev/js/script.js')
+      .pipe(browserSync.stream())
+      .pipe(gulp.dest('build/js'));
+}
+
+function imageMin() {
+   return gulp.src([
+      '#dev/img/**/*.{jpg,jpeg,gif,png,svg}',
+      '!#dev/img/sprites/*',])
+      .pipe(gulpImageMin([
+         gulpImageMin.gifsicle({ interlaced: true }),
+         gulpImageMin.mozjpeg({ quality: 75, progressive: true }),
+         gulpImageMin.optipng({ optimizationLevel: 5 }),
+         gulpImageMin.svgo({
+            plugins: [
+               { removeViewBox: true },
+               { cleanupIDs: false }
+            ]
+         })
+      ]))
+      .pipe(gulp.dest('build/img/'))
+}
+
+function watch (){
    browserSync.init({
       server: {
-         baseDir: 'app/'
-      }
+         baseDir: "build"
+      },
+      notify: false
    });
+
+   gulp.watch('#dev/*.html', html);
+   gulp.watch('#dev/scss/*.scss', scss2css);
+   gulp.watch('#dev/img/**/*.{jpg,jpeg,gif,png,svg}', imageMin);
+   gulp.watch('#dev/js/*.js', script);
+   gulp.watch('build/*.html').on('change', browserSync.reload);
 }
 
-function cleanDist () {
-   return del('dist')
-}
-
-function images() {
-   return src('app/img/**/*')
-      .pipe(imagemin(
-         [
-            imagemin.gifsicle({ interlaced: true }),
-            imagemin.mozjpeg({ quality: 75, progressive: true }),
-            imagemin.optipng({ optimizationLevel: 5 }),
-            imagemin.svgo({
-               plugins: [
-                  { removeViewBox: true },
-                  { cleanupIDs: false }
-               ]
-            })
-         ]
-      ))
-      .pipe(dest('dist/img'))
-}
-
-function styles() {
-   return src('app/scss/style.scss')
-      .pipe(scss({outputStyle: 'expanded'}))
-      .pipe(autoprefixer({
-         overrideBrowserslist: ['last 10 version'],
-         grid: true
-      }))
-      .pipe(dest('app/css'))
-      .pipe(browserSync.stream())
-}
-
-function scripts() {
-   return src('app/js/main.js')
-      .pipe(browserSync.stream())
-}
-
-function build() {
-   return src([
-      'app/css/style.css',
-      'app/fonts/**/*',
-      'app/js/main.js',
-      'app/*.html',
-   ], {base: 'app'})
-      .pipe(dest('dist'))
-}
-
-function watching() {
-   watch(['app/scss/**/*.scss'], styles);
-   watch(['app/js/*.js'], scripts);
-   watch(['app/*.html']).on('change', browserSync.reload);
-}
-
-
-exports.styles = styles;
-exports.scripts = scripts;
-exports.watching = watching;
-exports.browsersync = browsersync;
-exports.images = images;
-exports.cleanDist = cleanDist;
-
-exports.build = series(cleanDist, images, build);
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = gulp.series(delBuild, fonts, html, normalize, scss2css, imageMin, script, watch);
